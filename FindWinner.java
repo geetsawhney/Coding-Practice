7ec673ed6dc98d6c51bdf0766c7c975303ba70e5
Geet Sawhney <geetsawhney@Geets-MacBook-Pro.local>
2018-03-02 21:16:39 -0500

    Find the election winner

---
+++ FindWinner.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindWinner {
+
+	public static String electionWinner(String[] votes) {
+		Map<String, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<votes.length;i++) {
+			if(map.containsKey(votes[i])) {
+				map.put(votes[i], map.get(votes[i])+1);
+			}else {
+				map.put(votes[i], 1);
+			}
+		}
+		
+		int maxVotes = 0;
+		String winner = "";
+		for(String s: map.keySet()) {
+			if(map.get(s) > maxVotes) {
+					winner = s;
+					maxVotes = map.get(s);
+			}else if(map.get(s) == maxVotes) {
+				if(winner.compareTo(s) > 0) {
+					winner = s;
+				}
+			}
+		}
+		return winner;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] votes= {"Ron", "Jack", "Ron","Jack"};
+		
+		System.out.println(electionWinner(votes));
+
+	}
+
+}
