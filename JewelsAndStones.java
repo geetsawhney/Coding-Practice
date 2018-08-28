3e0491d2551c4b1f6a050a0a627505b7191b8198
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-08-28 00:32:46 -0700

    Adding better approach for jewels and stones

--- JewelsAndStones.java
+++ JewelsAndStones.java
@@ -17,20 +17,20 @@ public class JewelsAndStones {
 		return ans;
 	}
 
-	// public static int numJewelsInStones_1(String J, String S) {
-	//
-	// 	boolean check[]=new boolean[128];
-	// 	int ans=0;
-	// 	for(int i=0;i<J.length();i++) {
-	// 		check[J.charAt(i)]=true;
-	// 	}
-	//
-	// 	for(int i=0;i<S.length();i++) {
-	// 		if(check[S.charAt(i)])
-	// 			ans++;
-	// 	}
-	// 	return ans;
-	// }
+	public static int numJewelsInStones_1(String J, String S) {
+
+		boolean check[]=new boolean[128];
+		int ans=0;
+		for(int i=0;i<J.length();i++) {
+			check[J.charAt(i)]=true;
+		}
+
+		for(int i=0;i<S.length();i++) {
+			if(check[S.charAt(i)])
+				ans++;
+		}
+		return ans;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
