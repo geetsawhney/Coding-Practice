86b4caa053e1610a226ea98566e87fa6e63bf081
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-09-16 17:13:20 -0400

    Inplace changes for even before odd

--- EvenBeforeOdd.java
+++ EvenBeforeOdd.java
@@ -1,4 +1,3 @@
-
 // Leetcode 905
 class EvenBeforeOdd {
     public static int[] sortArrayByParity(int[] A) {
@@ -14,9 +13,32 @@ class EvenBeforeOdd {
         return out;
     }
 
+    public static int[] sortArrayByParity_1(int[] A) {
+        int i=0, j=A.length-1;
+        int temp;
+        while(i<j) {
+        	if(A[i]%2!=0 && A[j]%2==0) {
+        		temp=A[i];
+        		A[i]=A[j];
+        		A[j]=temp;
+        		i++;
+        		j--;
+        	}
+        	if(i>=j)
+        	 return A;
+        	if(A[i]%2==0)
+        		i++;
+        	if(A[j]%2!=0)
+        		j--;
+        }
+
+        return A;
+    }
+
+
     public static void main(String[] args) {
     	int[] input= {1,2,3,4,5,6,7};
-    	input=sortArrayByParity(input);
+    	input=sortArrayByParity_1(input);
     	for(int n: input) {
 			System.out.println(n);
 		}
