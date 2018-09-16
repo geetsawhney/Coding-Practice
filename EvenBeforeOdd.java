ffc74b99b41a06d173c8562549933c12e73f4ad3
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-09-16 16:48:56 -0400

    Sort by parity

---
+++ EvenBeforeOdd.java
@@ -0,0 +1,23 @@
+
+class EvenBeforeOdd {
+    public static int[] sortArrayByParity(int[] A) {
+        int i=0, j=A.length-1, out[]=new int[A.length];
+        for(int x: A){
+            if(x%2==0){
+                out[i++]=x;
+            }
+            else{
+                out[j--]=x;
+            }
+        }
+        return out;
+    }
+
+    public static void main(String[] args) {
+    	int[] input= {1,2,3,4,5,6,7};
+    	input=sortArrayByParity(input);
+    	for(int n: input) {
+			System.out.println(n);
+		}
+	}
+}
