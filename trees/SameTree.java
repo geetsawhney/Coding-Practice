45b4ab28f16ed4f7dee2a8ba114209addce4bd7c
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-09-26 01:34:36 -0400

    Check if 2 trees are same

---
+++ trees/SameTree.java
@@ -0,0 +1,25 @@
+package trees;
+// Definition for a binary tree node.
+
+class SameTree {
+	public static boolean isSameTree(TreeNode p, TreeNode q) {
+		if(p==q)
+			return true;
+
+		if(p==null || q==null)
+			return false;
+
+		if(p.val!=q.val)
+			return false;
+
+		return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+	}
+
+	public static void main(String args[]) {
+
+		TreeNode p=new TreeNode(10);
+		TreeNode q=new TreeNode(10);
+		q.left=new TreeNode(10);
+		System.out.println(isSameTree(p, q));
+	}
+}
