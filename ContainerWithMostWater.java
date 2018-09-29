ca3d793d5a7755d74a5efd78f03da50bd32daf26
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-09-29 03:55:19 -0400

    Container with most water

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+
+public class ContainerWithMostWater {
+
+	public static int maxArea(int[] height) {
+		int start=0;
+		int end=height.length-1;
+		int maxVal=Integer.MIN_VALUE;
+		while(start<end) {
+			maxVal=Math.max(maxVal, (Math.min(height[start], height[end]))*(end-start));
+			if(height[start] < height[end]) {
+				start ++;
+			}else {
+				end--;
+			}
+		}
+		return maxVal;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(maxArea(new int[] {1,8,6,2,5,4,8,3,7}));
+
+	}
+
+}
