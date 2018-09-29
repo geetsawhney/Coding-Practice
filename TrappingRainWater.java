775b42275bf08d7cca0b08abb2a675b227fbd026
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-09-29 02:31:22 -0400

    Find the amount of water trapped

---
+++ TrappingRainWater.java
@@ -0,0 +1,35 @@
+
+public class TrappingRainWater {
+
+	public static int trap(int[] height) {
+		int ans=0;
+
+		int len=height.length-1;
+		int[] left=new int[len+1];
+		int[] right=new int[len+1];
+
+		left[0]=height[0];
+		right[len]=height[len];
+
+		for(int i=1; i<=len;i++) {
+			left[i]=Math.max(left[i-1], height[i]);
+			right[len-i]=Math.max(right[len-i+1], height[len-i]);
+		}
+
+		for(int i=0;i<=len;i++) {
+			ans+=Math.min(left[i], right[i]) - height[i];
+		}
+
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] height= {0,1,0,2,1,0,1,3,2,1,2,1};
+		System.out.println(trap(height));
+
+
+	}
+
+}
