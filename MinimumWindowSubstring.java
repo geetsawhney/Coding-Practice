9794d09d9a382db5d4ed2d4ffba37bcfc57c8bc3
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-10-07 22:14:59 -0400

    Adding comments and approach to solve the problem

--- MinimumWindowSubstring.java
+++ MinimumWindowSubstring.java
@@ -3,16 +3,19 @@ public class MinimumWindowSubstring {
 
 	public static String minWindow(String s, String t) {
 
-		int remaining=t.length();
-		int start=0;
-		int end=0;
-		int minStart=0;
-		int minEnd=0;
-		int minLength=Integer.MAX_VALUE;
 
+		int remaining=t.length();	// remaining elements required to tell if a substring matches or not
+		int start=0;	// current start index of substringh being checked
+		int end=0;	// current end index
+		int minStart=0;	// start index of substring that fulfills the requirements
+		int minEnd=0;	// end index of a substring that matches
+		int minLength=Integer.MAX_VALUE;	//to keep track of minLength answer
+
+		// if t is of size 0 or length of s is less than t
 		if(remaining==0 || s.length()<t.length())
 			return "";
 
+		// if only one element in t, then return "" if found else return t
 		if(t.length()==1) {
 			int pos= s.indexOf(t);
 			if(pos==-1)
@@ -20,18 +23,23 @@ public class MinimumWindowSubstring {
 			return t;
 		}
 
-		int[] required=new int[256];
-		int[] found=new int[256];
 
+		int[] required=new int[256]; //keep track of characters required to fulfill
+		int[] found=new int[256];  //keep track of elements currently being examined
+
+		// adding all elements frequency to required
 		for(int i=0;i<t.length();i++) {
 			required[t.charAt(i)]++;
 		}
 
 
 		char c;
+		// run loop till end of string or remaining elements equal to zero
+		// we expand to right and when find our required characters, we shring from
+		// left to minimize the substring length that matches the requirements
 		while(end<s.length() || remaining==0) {
 
-
+			// if remaining is 0, then we shrink from left, by incrementing start
 			if(remaining==0) {
 				c=s.charAt(start);
 				start++;
@@ -43,6 +51,7 @@ public class MinimumWindowSubstring {
 				}
 
 			}
+			// else we expand to right, by incrementing end
 			else {
 				c=s.charAt(end);
 				if(required[c]!=0) {
@@ -54,6 +63,7 @@ public class MinimumWindowSubstring {
 				end++;
 			}
 
+			// when requirements match and length of current substring is less than last matching substring
 			if(remaining==0 && minLength>(end-start)) {
 				minLength=end-start;
 				minStart=start;
