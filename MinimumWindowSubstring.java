b7eb67fcad87902a165463b9f9d6e767a8b0dc50
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-10-07 22:01:33 -0400

    Finally!! Minimum window substring done

---
+++ MinimumWindowSubstring.java
@@ -0,0 +1,76 @@
+
+public class MinimumWindowSubstring {
+
+	public static String minWindow(String s, String t) {
+
+		int remaining=t.length();
+		int start=0;
+		int end=0;
+		int minStart=0;
+		int minEnd=0;
+		int minLength=Integer.MAX_VALUE;
+
+		if(remaining==0 || s.length()<t.length())
+			return "";
+
+		if(t.length()==1) {
+			int pos= s.indexOf(t);
+			if(pos==-1)
+				return "";
+			return t;
+		}
+
+		int[] required=new int[256];
+		int[] found=new int[256];
+
+		for(int i=0;i<t.length();i++) {
+			required[t.charAt(i)]++;
+		}
+
+
+		char c;
+		while(end<s.length() || remaining==0) {
+
+
+			if(remaining==0) {
+				c=s.charAt(start);
+				start++;
+
+				if(found[c]!=0) {
+					found[c]--;
+					if(found[c]<required[c])
+						remaining++;
+				}
+
+			}
+			else {
+				c=s.charAt(end);
+				if(required[c]!=0) {
+					found[c]++;
+					if(found[c]<=required[c]) {
+						remaining--;
+					}
+				}
+				end++;
+			}
+
+			if(remaining==0 && minLength>(end-start)) {
+				minLength=end-start;
+				minStart=start;
+				minEnd=end;
+			}
+
+		}
+
+		return minLength==Integer.MAX_VALUE?"":s.substring(minStart,minEnd);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+
+	}
+
+}
