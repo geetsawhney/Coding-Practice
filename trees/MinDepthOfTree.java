2a031840352a763732c5cbeb34547ff6bb6aa6c6
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-11-02 01:03:55 -0400

    Min depth of tree

---
+++ trees/MinDepthOfTree.java
@@ -0,0 +1,24 @@
+package trees;
+
+public class MinDepthOfTree {
+
+	public static int minDepth(TreeNode root) {
+		if(root == null)
+			return 0;
+
+		if(root.left == null && root.right==null)
+			return 1;
+		if(root.left == null)
+			return 1+ minDepth(root.right);
+		if(root.right==null)
+			return 1 + minDepth(root.left);
+
+		return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
