9416020c32943c26d805e9362dc7d0189d1b3d60
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-11-03 03:15:04 -0400

    Program to flatten a bonary tree

---
+++ trees/FlattenBinaryTree.java
@@ -0,0 +1,31 @@
+class FlattenBinaryTree {
+    public void flatten(TreeNode root) {
+
+        flattenHelp(root);
+
+    }
+
+     public TreeNode flattenHelp(TreeNode root) {
+        if(root == null)
+            return null;
+        TreeNode leftChild = flattenHelp(root.left);
+        TreeNode rightChild = flattenHelp(root.right);
+        root.left=null;
+         if(leftChild == null)
+         {
+             root.right = rightChild;
+             return root;
+         }
+         root.right = leftChild;
+
+         if(rightChild == null)
+             return root;
+
+         while(leftChild.right!=null){
+             leftChild=leftChild.right;
+         }
+         leftChild.right=rightChild;
+
+         return root;
+    }
+}
