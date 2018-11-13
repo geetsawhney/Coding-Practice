06c1e35af5bd136b9f83ec8d995ee158a38ad885
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-11-13 18:27:21 -0500

    removing redundant conditions

--- FindPivotIndex.java
+++ FindPivotIndex.java
@@ -1,35 +1,26 @@
 
 //leetcode 724
 public class FindPivotIndex {
-    public static int pivotIndex(int[] nums) {
+	public static int pivotIndex(int[] nums) {
 
-        int[] leftSum= new int[nums.length];
-        int[] rightSum= new int[nums.length];
+		int[] leftSum= new int[nums.length];
+		int[] rightSum= new int[nums.length];
 
-        for(int i=1;i<nums.length;i++){
-            leftSum[i] = leftSum[i-1]+nums[i-1];
-            rightSum[nums.length-i-1] = rightSum[nums.length-i]+ nums[nums.length-i];
-        }
+		for(int i=1;i<nums.length;i++){
+			leftSum[i] = leftSum[i-1]+nums[i-1];
+			rightSum[nums.length-i-1] = rightSum[nums.length-i]+ nums[nums.length-i];
+		}
 
-        for(int i=0;i<nums.length;i++){
+		for(int i=0;i<nums.length;i++){
+			if(leftSum[i]==rightSum[i])
+				return i;
+		}
+		return -1;
+	}
 
-            if(i==0){
-                if(rightSum[i]==0)
-                    return i;
-            }else if(i==nums.length-1){
-                if(leftSum[i]==0)
-                    return i;
-            }else{
-                if(leftSum[i]==rightSum[i])
-                    return i;
-            }
-        }
-
-        return -1;
-    }
-
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(pivotIndex(new int[] {3,1,1,1,3}));
 
 	}
 }
