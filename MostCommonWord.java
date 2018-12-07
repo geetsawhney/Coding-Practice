9c1429d64362ad088bd260282ddefc8bf415a148
Geet Sawhney <geet_sawhney14@yahoo.co.in>
2018-12-07 00:23:22 -0500

    Find the most common word not in wordlist

---
+++ MostCommonWord.java
@@ -0,0 +1,38 @@
+package assessment;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MostCommonWord {
+
+	public String mostCommonWord(String paragraph, String[] banned) {
+
+		String[] words = paragraph.split("[\\s\\.,!;'?]+");
+		Set<String> ban = new HashSet<>();
+		ban.addAll(Arrays.asList(banned));
+		Map<String, Integer> map=new HashMap<>();
+		int ansCount=0;
+		String ans = null;
+
+		for(String w:words) {
+			w=w.toLowerCase();
+			if(!ban.contains(w)){
+				map.put(w, map.getOrDefault(w, 0)+1);
+				if(ansCount<map.get(w)) {
+					ans=w;
+					ansCount=map.get(w);
+				}
+			}
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
